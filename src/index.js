654c47f3cf3875e6d9ee399c29828408af5f2802
kamalnamazov <kamaln788@gmail.com>
2024-12-26 22:53:35 +0400

    final commit3

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { GlobalContextProvider } from './context/global';
 import GlobalStyle from './Globalstyle';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
