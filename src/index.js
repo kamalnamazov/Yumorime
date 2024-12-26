3b55dc259c30a5a26cb94346b5c44e1e272db001
kamalnamazov <kamaln788@gmail.com>
2024-12-26 22:35:57 +0400

    final commit

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GlobalContextProvider } from './context/global';
 import GlobalStyle from './Globalstyle';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter basename='Yumorime'>
   <React.StrictMode>
     <GlobalStyle />
     <GlobalContextProvider>
       <App />
     </GlobalContextProvider>
   </React.StrictMode>
+  </BrowserRouter>
 );
