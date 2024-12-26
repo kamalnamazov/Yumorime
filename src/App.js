1041bbfdfdd2109ffd3c7fa33cae2bd6e757a06c
kamalnamazov <kamaln788@gmail.com>
2024-12-26 23:04:53 +0400

    final commit4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AnimeItem from "./Components/AnimeItem";
 import Homepage from "./Components/Homepage";
+import Layout from "./Components/Layout";
+import GlobalStyle from "./Globalstyle";
+import Popular from "./Components/Popular";
+import Spotlight from "./Components/Spotlight";
+import globalContext from "./context/global";
 
 function App() {
   
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import { GlobalContextProvider } from './context/global';
 import GlobalStyle from './Globalstyle';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
